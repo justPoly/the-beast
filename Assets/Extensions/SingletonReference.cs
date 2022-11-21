c98fe86d9da9507dd6ac4ebec3f83523d98b9477
Polycarp Atalor <polycarpatalor@gmail.com>
2022-11-21 18:36:30 +0100

    Project file upload

---
+++ Assets/Extensions/SingletonReference.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SingletonReference : MonoBehaviour
+{
+    // public GameStateManager GameStateManager;
+
+
+    // private void Awake()
+    // {
+    //     GameStateManager.Init();
+    // }
+}
