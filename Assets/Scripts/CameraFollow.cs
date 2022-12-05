f69343dc58f9788205db9296c8ea50248a1c835a
MeMeee1 <82448794+MeMeee1@users.noreply.github.com>
2022-12-05 01:27:33 +0100

    Updated camera follow script.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,7 +12,7 @@ public class CameraFollow : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform target;
     
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         Vector3 targetPosition = target.position +offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothtime);
