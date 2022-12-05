86677d43a0a1e37908a0548b0ed820a4104fc946
Polycarp Atalor <polycarpatalor@gmail.com>
2022-12-05 11:55:50 +0100

    Camera Smoothing

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -18,7 +18,7 @@ public class CameraFollow : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         Vector3 targetPosition = target.position +offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothtime);
